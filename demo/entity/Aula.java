66250e03b276bce3e7db660f0f6202c7c88cdd0e
aaroncitto <agalbarrovicente@gmail.com>
2025-12-05 13:01:05 +0100

    Creacion de las clases "Main" y "ReservaService" y cambios en las clases "Aula" y "Reserva"

--- demo/entity/Aula.java
+++ demo/entity/Aula.java
@@ -2,13 +2,55 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
-
+@Entity
+@Table(name = "Aula")
 public class Aula {
 
-   
-    private Long id;
+   @Id
+   @GeneratedValue(strategy = Generation.Type.IDENTITY)
 
+   @Column(name = "id_aula")
+    private Long id;
+     @Column(name = "nombre")
+    private String nombre;
+     @Column(name = "capacidad")
+    private Integer capacidad;
+     @Column(name = "edificio")
+    private String edificio;
     // añadir mínimo 3 atributos (ejemplo: nombre, capacidad, edificio)
 
-  
+
+    //Getters setters
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Integer setCapacidad(){
+        return capacidad;
+    }
+
+    public void getCapacidad(String capacidad){
+        this.capacidad = capacidad;
+    }
+
+    public String getEdificio(){
+        return edificio;
+    }
+
+    public void setEdificio(){
+        this.edificio = edificio;
+    }
+        
 }
--- demo/entity/Reserva.java
+++ demo/entity/Reserva.java
@@ -4,13 +4,60 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "reserva")
 public class Reserva {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    
+     @Column(name = "id_reserva")
+      private Long id;
 
+     @Column(name = "fecha")
+      private LocalDate fecha;
+
+     @Column(name = "horaInicio")
+      private LocalTime horaInicio;
+
+     @Column(name = "horaFin")
+      private LocalTime horaFin;
+
+     @Column(name = "aulaId")
+      private Long aulaId;
     // añadir mínimo 3 atributos (ejemplo: fecha, horaInicio, horaFin, aulaId)
     // TODO: constructor con parámetros
+
+    //Getters setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public LocalDate getFecha(){
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha){
+        this.fecha = fecha;
+    }
+    public LocalTime getHoraInicio(){
+        return horaInicio;
+    }
+    public void setHoraInicio(LocalTime horaInicio){
+        this.horaInicio = horaInicio;
+    }
+    public LocalTime getHoraFin(){
+        return horaFin;
+    }
+    public void setHoraFin(LocalTime horaFin){
+        this.horaFin = horaFin;
+    }
+    public Long getAulaId(){
+        return aulaId;
+    }
+    public void setAulaId(){
+        this.aulaId = aulaId;
+    }
 
 }
