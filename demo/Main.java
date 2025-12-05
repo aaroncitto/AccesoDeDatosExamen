d9862ac5cb0765c148dbe3f4ea8a6672d96c1e5c
aaroncitto <agalbarrovicente@gmail.com>
2025-12-05 13:32:28 +0100

    Cambios en la clase Main

---
+++ demo/Main.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+}
